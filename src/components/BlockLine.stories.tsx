0745bd10d86850301dc9a791a1bc34d69a4efbef
Ulyana <lana.arfinova@gmail.com>
2024-03-05 20:43:26 +0300

    all .stories done

---
+++ src/components/BlockLine.stories.tsx
@@ -0,0 +1,8 @@
+import BlockLine from "./BlockLine";
+
+export default {
+    title: 'Block Line',
+    component: BlockLine,
+}
+
+export const Default = () => <BlockLine></BlockLine>
---
+++ src/components/CollapseBlock.stories.tsx
@@ -0,0 +1,10 @@
+import CollapseBlock from "./CollapseBlock";
+
+export default {
+    title: 'Collapse Block',
+    component: CollapseBlock,
+}
+
+export const Default = () => <CollapseBlock heading="Схлопывающийся блок">
+Вот такой тут внутри интереснейший контент.
+</CollapseBlock>
